95d9c0f61fcadc737d4a3d4ea9de3b8b6219f128
Rajat Sran <rajatsran27@gmail.com>
2021-07-15 18:15:53 +0530

    remove function added , also used chalk

--- app.js
+++ app.js
@@ -22,9 +22,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        // console.log('Title: ' + argv.title)
-        // console.log('Body: ' + argv.body)
-        notes.addNote(argv.title,argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -32,8 +30,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
     }
 })
 
