c9d10c5c3fa2245fc7c52c31777888b5ae3180cc
Rajat Sran <rajatsran27@gmail.com>
2021-07-15 16:13:40 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const getNotes = function () {
+    return 'your notes...'
+}
+const addNote = function (title, body) {
+
+}
+const loadNotes = function () {
+
+}
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
